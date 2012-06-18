b0470d29f7bb2a4f136d8fd03ac419719b93ceab
Thomas <mail@lokalnetz.com>
2012-06-18 11:10:45 +0300

    Update master

--- cpu.php
+++ cpu.php
@@ -10,25 +10,3 @@
     }
     print($cpuUsage);
 ?>
-
-<?php
-
-//$stat1 = file('/proc/stat');
-//sleep(1);
-//$stat2 = file('/proc/stat');
-//$info1 = explode(" ", preg_replace("!cpu +!", "", $stat1[0]));
-//$info2 = explode(" ", preg_replace("!cpu +!", "", $stat2[0]));
-//$dif = array();
-//$dif['user'] = $info2[0] - $info1[0];
-//$dif['nice'] = $info2[1] - $info1[1];
-//$dif['sys'] = $info2[2] - $info1[2];
-//$dif['idle'] = $info2[3] - $info1[3];
-//$total = array_sum($dif);
-//$cpu = array();
-//foreach($dif as $x=>$y) $cpu[$x] = round($y / $total * 100, 1);
-//
-//echo "IDLE :" . $cpu['idle'];
-//echo "CPU  :" . $cpu['user'];
-//print_r(file('/proc/stat'));
-//
-?>
